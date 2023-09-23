23e33171cd0da61f7eef800ec78b0e8d3250a9eb
alifmaulana24 <alifmaulana1144@gmail.com>
2023-09-23 12:07:19 +0700

    first commit

---
+++ callback.js
@@ -0,0 +1,32 @@
+// const mahasiswa = [
+//     {
+//         nama : 'muhammad alif maulana',
+//         jurusan : 'farmasi'
+//     },
+//     {
+//         nama : 'ferdi anggara',
+//         jurusan : 'informatika'
+//     },
+//     {
+//         nama : ' rizan sanjaya',
+//         jurusan : 'polisi'
+//     },
+// ];
+
+
+// const nama = 'alif maulana';
+// const umur = 12
+// const email = 'alifmaulana@gmail.com'
+
+// function test(string,...value) {
+//    return string.reduce((res,str,i) => ` ${res}${str} <span class='hl' > ${value[i] || ''} </span>`,'')
+// }
+
+// const aran =test`halo nama saya ${nama}, umur saya ${umur}, email saya ${email}`
+// console.log(aran);
+
+// document.body.textContent = `<h1> hahaha i hacked you</h1>`
+
+
+
+
